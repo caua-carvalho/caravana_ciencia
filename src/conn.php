6384263e56d68d17075123038abd9b2b08f24803
SAO JOSE DOS CAMPOS - ELIDIA TEDESCO DE OLIVEIRA PROFESSORA <e920459w10@professor.educacao.sp.gov.br>
2025-09-11 11:48:02 -0300

    conexao BD

--- src/conn.php
+++ src/conn.php
@@ -1,9 +1,9 @@
 <?php
 // Configuração do banco de dados
-define('DB_HOST', 'sql103.infinityfree.com');
-define('DB_USER', 'if0_39917407');
-define('DB_PASS', '9H5pGekYWdHUF');
-define('DB_NAME', 'if0_39917407_caravana');
+define('DB_HOST', 'mysql.railway.internal');
+define('DB_USER', 'root');
+define('DB_PASS', 'aUHjWxtaJwslkOVdYPVrqrgnXFCNHsQF');
+define('DB_NAME', 'railway');
 
 $conexao = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
